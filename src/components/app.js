99b69917c48401d7e6b0885ab300ef111a8c4220
jas-dev <jaysamora@outlook.com>
2019-03-31 14:46:49 -0700

    React functional components - Jay Samora

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react';
+import Table from './table';
 
+const students = [
+    {
+        student: 'Bob Williams',
+        course: 'Woodshop',
+        grade: 100
+    },
+    {
+        student: 'Bif Biffler',
+        course: 'Tennis',
+        grade: 99
+    },
+    {
+        student: 'Patty Pleasant',
+        course: 'Pleasantries',
+        grade: 89
+    }
+];
+
+const App=()=>(
+    <div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data={students}/>
+    </div>
+);
+
+export default App;
 
