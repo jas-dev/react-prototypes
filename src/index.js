e409f5617f4092043edc46befa9d2d0fe70483d3
jas-dev <jaysamora@outlook.com>
2019-03-31 12:51:45 -0700

    React intro prototype - Jay samora

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function luckyNumber(){
+    return Math.floor(Math.random()*(1000+1));
+}
+
+const user = {
+    name : 'Bob',
+    luckyNumber: luckyNumber()
+};
+const Greeting = ()=>(
+    <div className= 'container'>
+        <h1>Hello {user.name}</h1>
+        <h2 className= 'text-muted'>{user.name}, your lucky number is: {user.luckyNumber}</h2>
+    </div>
+);
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <Greeting/>,
     document.getElementById('root')
 );
+
