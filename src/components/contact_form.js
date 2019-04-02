e65de18c9e767ea03a10974c8bae1ace836a580e
jas-dev <jaysamora@outlook.com>
2019-04-02 10:25:47 -0700

    feature set 7 complete

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -14,6 +14,7 @@ class ContactForm extends Component{
         };
         this.handleInputChange=this.handleInputChange.bind(this);
         this.handleSubmit=this.handleSubmit.bind(this);
+        this.reset=this.reset.bind(this);
     }
     handleInputChange(event){
         const {form} = this.state;
@@ -25,8 +26,19 @@ class ContactForm extends Component{
         event.preventDefault();
         console.log('handle submit called, form values are:',this.state.form);
         this.props.add(this.state.form);
+        this.reset();
 
 
+    }
+    reset(){
+        this.setState({
+            form: {
+                firstName: '',
+                lastName: '',
+                phone: '',
+                email: ''
+            }
+        })
     }
     render(){
         const {firstName, lastName, phone, email} = this.state.form;
@@ -38,6 +50,7 @@ class ContactForm extends Component{
               <Field name='phone' label='Phone' type='tel' value={phone} onChange={this.handleInputChange}/>
               <Field name='email' label='Email' type='email' value={email} onChange={this.handleInputChange}/>
              <button>Add Contact</button>
+              <button type='button' onClick={this.reset}>Clear Forms</button>
           </form>
         );
     }
