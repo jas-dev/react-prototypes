fc746aa35f539156d9c98089519e0689ab0a136f
jas-dev <jaysamora@outlook.com>
2019-04-10 07:08:39 -0700

    featureset 1 complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,16 @@
+import React, {Component} from 'react';
+import './app.css'
 
+class App extends Component{
+    render(){
+
+        return(
+            <div className='main-container'>
+                <h1 className='main-title'>Scrapbook</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
 
